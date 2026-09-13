ca7f91ba30fc8720de481b005e5145ee3c3887cf
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-13 17:15:26 -0400

    feat: add bounded operating path package

---
+++ devintel/operations/__init__.py
@@ -0,0 +1,5 @@
+"""Bounded end-to-end operating paths."""
+
+from .bounded import BoundedOperation, BoundedOperationEngine, OperationResult
+
+__all__ = ["BoundedOperation", "BoundedOperationEngine", "OperationResult"]
