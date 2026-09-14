00ee8ea6b93a8071ffd3146ceb23908b84be5f51
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-14 10:22:40 -0400

    feat: export outcome-aware routing service

--- devintel/executive/__init__.py
+++ devintel/executive/__init__.py
@@ -4,6 +4,7 @@ from .engine import DefaultGoalInterpreter, ExecutiveEngine, ExecutiveResult, Ex
 from .evidence import EvidenceBackedExecutiveAdapter, EvidenceBackedUnderstanding, EvidenceRequirementError
 from .routing import CollaborationPlan, CollaborationStep, RouteCandidate, RoutingRequirement, SpecialistDescriptor, SpecialistKind, SpecialistRouter
 from .nl_goal import BoundedNaturalLanguageGoalBoundary, DeterministicNaturalLanguageInterpreter, GoalInterpretation, NaturalLanguageGoalInterpreter
+from .outcome_routing import OutcomeAwareRoutingService
 
 __all__ = [
     "DefaultGoalInterpreter", "ExecutiveEngine", "ExecutivePlan", "ExecutiveResult",
@@ -12,4 +13,5 @@ __all__ = [
     "CollaborationPlan", "CollaborationStep", "RouteCandidate", "RoutingRequirement",
     "SpecialistDescriptor", "SpecialistKind", "SpecialistRouter",
     "BoundedNaturalLanguageGoalBoundary", "DeterministicNaturalLanguageInterpreter", "GoalInterpretation", "NaturalLanguageGoalInterpreter",
+    "OutcomeAwareRoutingService",
 ]
