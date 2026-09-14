ddedfb042e63c594c442c0a341f0e62dfe1da59a
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-14 16:17:27 -0400

    feat: export language intelligence

---
+++ devintel/modules/language/__init__.py
@@ -0,0 +1,5 @@
+"""Language and communication intelligence contracts."""
+
+from .intelligence import LanguageIntelligence, LanguageUnderstanding, ResponseConstraints
+
+__all__ = ["LanguageIntelligence", "LanguageUnderstanding", "ResponseConstraints"]
