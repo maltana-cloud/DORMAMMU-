a3a09ddf480c29bf4db6e206e4de2cb94b54a1e6
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-14 09:54:25 -0400

    feat: export durable learning store

--- devintel/autonomy/__init__.py
+++ devintel/autonomy/__init__.py
@@ -3,5 +3,6 @@ from .contracts import AutonomousCycle, AutonomyPhase, Observation
 from .engine import AutonomousEngine
 from .store import AutonomousCycleStore
 from .learning import OutcomeEvidence, LearningProposal, OutcomeLearner
+from .learning_store import LearningStore
 
-__all__ = ["AutonomousCycle", "AutonomyPhase", "Observation", "AutonomousEngine", "AutonomousCycleStore", "OutcomeEvidence", "LearningProposal", "OutcomeLearner"]
+__all__ = ["AutonomousCycle", "AutonomyPhase", "Observation", "AutonomousEngine", "AutonomousCycleStore", "OutcomeEvidence", "LearningProposal", "OutcomeLearner", "LearningStore"]
