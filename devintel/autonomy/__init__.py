492fccc56d9f53afd1ab142f1048b81044ad5dec
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-14 09:54:08 -0400

    feat: export outcome learning contracts

--- devintel/autonomy/__init__.py
+++ devintel/autonomy/__init__.py
@@ -1,6 +1,7 @@
-"""Bounded autonomous operating loop."""
+"""Bounded autonomous operating and learning loop."""
 from .contracts import AutonomousCycle, AutonomyPhase, Observation
 from .engine import AutonomousEngine
 from .store import AutonomousCycleStore
+from .learning import OutcomeEvidence, LearningProposal, OutcomeLearner
 
-__all__ = ["AutonomousCycle", "AutonomyPhase", "Observation", "AutonomousEngine", "AutonomousCycleStore"]
+__all__ = ["AutonomousCycle", "AutonomyPhase", "Observation", "AutonomousEngine", "AutonomousCycleStore", "OutcomeEvidence", "LearningProposal", "OutcomeLearner"]
