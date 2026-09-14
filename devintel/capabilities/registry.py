62c36ef13b33e4d21e58ce84cd0360a0d3899515
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-14 09:29:43 -0400

    fix: remove capability store import cycle

--- devintel/capabilities/registry.py
+++ devintel/capabilities/registry.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 
 from threading import RLock
 from .contracts import CapabilityDescriptor, CapabilityGap, ResourceDescriptor
-from .store import CapabilityRegistryStore
 
 
 class CapabilityRegistry:
