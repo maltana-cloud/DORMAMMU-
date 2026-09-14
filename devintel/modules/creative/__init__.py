2067d9282d4e7dbf3144dec6c0af42d127fb230b
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-14 17:02:36 -0400

    feat: expose creative lineage store

--- devintel/modules/creative/__init__.py
+++ devintel/modules/creative/__init__.py
@@ -1,10 +1,6 @@
 from .contracts import AssetKind, CreativeBrief, CreativeConcept, CreativeConsistency, CreativeEvaluation, CreativePlan, CreativeStatus, brief_digest, plan_digest
 from .intelligence import CreativeIntelligence
+from .lineage import CreativeLineageRecord, CreativeLineageStore
 from .pipeline import CreativeArtifact, CreativePipeline, CreativeResult
 from .providers import CreativeProvider, CreativeProviderRegistry, ProviderSelection
-
-__all__ = [
-    "AssetKind", "CreativeBrief", "CreativeConcept", "CreativeConsistency", "CreativeEvaluation", "CreativePlan", "CreativeStatus",
-    "CreativeIntelligence", "CreativeArtifact", "CreativePipeline", "CreativeResult",
-    "CreativeProvider", "CreativeProviderRegistry", "ProviderSelection", "brief_digest", "plan_digest",
-]
+__all__ = ["AssetKind", "CreativeBrief", "CreativeConcept", "CreativeConsistency", "CreativeEvaluation", "CreativePlan", "CreativeStatus", "CreativeIntelligence", "CreativeArtifact", "CreativePipeline", "CreativeResult", "CreativeLineageRecord", "CreativeLineageStore", "CreativeProvider", "CreativeProviderRegistry", "ProviderSelection", "brief_digest", "plan_digest"]
