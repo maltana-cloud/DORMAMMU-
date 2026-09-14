90b95a1ebe1b943fcdd64ddc0819489be1e2a969
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-14 17:25:53 -0400

    feat(creative): export verification contracts

--- devintel/modules/creative/__init__.py
+++ devintel/modules/creative/__init__.py
@@ -3,4 +3,5 @@ from .intelligence import CreativeIntelligence
 from .lineage import CreativeLineageRecord, CreativeLineageStore
 from .pipeline import CreativeArtifact, CreativePipeline, CreativeResult
 from .providers import CreativeProvider, CreativeProviderRegistry, ProviderSelection
-__all__ = ["AssetKind", "CreativeBrief", "CreativeConcept", "CreativeConsistency", "CreativeEvaluation", "CreativePlan", "CreativeStatus", "CreativeIntelligence", "CreativeArtifact", "CreativePipeline", "CreativeResult", "CreativeLineageRecord", "CreativeLineageStore", "CreativeProvider", "CreativeProviderRegistry", "ProviderSelection", "brief_digest", "plan_digest"]
+from .verification import CreativeSafetyGate, CreativeVerification, CreativeVerifier
+__all__ = ["AssetKind", "CreativeBrief", "CreativeConcept", "CreativeConsistency", "CreativeEvaluation", "CreativePlan", "CreativeStatus", "CreativeIntelligence", "CreativeArtifact", "CreativePipeline", "CreativeResult", "CreativeLineageRecord", "CreativeLineageStore", "CreativeProvider", "CreativeProviderRegistry", "ProviderSelection", "CreativeSafetyGate", "CreativeVerification", "CreativeVerifier", "brief_digest", "plan_digest"]
