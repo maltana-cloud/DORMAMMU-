f1e203c4a75600fd9b82e81df88f3678f7d2faec
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-14 17:29:03 -0400

    feat(creative): export creation request adapter

--- devintel/modules/creative/__init__.py
+++ devintel/modules/creative/__init__.py
@@ -1,8 +1,9 @@
 from .contracts import AssetKind, CreativeBrief, CreativeConcept, CreativeConsistency, CreativeEvaluation, CreativePlan, CreativeStatus, brief_digest, plan_digest
 from .context import CreativeContext, CreativeContextAdapter
+from .creation import CreativeCreationAdapter, CreativeCreationRequest
 from .intelligence import CreativeIntelligence
 from .lineage import CreativeLineageRecord, CreativeLineageStore
 from .pipeline import CreativeArtifact, CreativePipeline, CreativeResult
 from .providers import CreativeProvider, CreativeProviderRegistry, ProviderSelection
 from .verification import CreativeSafetyGate, CreativeVerification, CreativeVerifier
-__all__ = ["AssetKind", "CreativeBrief", "CreativeConcept", "CreativeConsistency", "CreativeEvaluation", "CreativePlan", "CreativeStatus", "CreativeIntelligence", "CreativeArtifact", "CreativePipeline", "CreativeResult", "CreativeLineageRecord", "CreativeLineageStore", "CreativeProvider", "CreativeProviderRegistry", "ProviderSelection", "CreativeSafetyGate", "CreativeVerification", "CreativeVerifier", "CreativeContext", "CreativeContextAdapter", "brief_digest", "plan_digest"]
+__all__ = ["AssetKind", "CreativeBrief", "CreativeConcept", "CreativeConsistency", "CreativeEvaluation", "CreativePlan", "CreativeStatus", "CreativeIntelligence", "CreativeArtifact", "CreativePipeline", "CreativeResult", "CreativeLineageRecord", "CreativeLineageStore", "CreativeProvider", "CreativeProviderRegistry", "ProviderSelection", "CreativeSafetyGate", "CreativeVerification", "CreativeVerifier", "CreativeContext", "CreativeContextAdapter", "CreativeCreationAdapter", "CreativeCreationRequest", "brief_digest", "plan_digest"]
