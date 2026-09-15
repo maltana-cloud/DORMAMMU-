8d7c152823c0c492f0e9a6d58062432c8d997d51
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-15 10:00:14 -0400

    Ω: expose dynamic authority controls

--- devintel/control/__init__.py
+++ devintel/control/__init__.py
@@ -1,5 +1,6 @@
-"""Owner control-center boundaries for DEVINTEL."""
+"""Owner control-center boundaries for DORMAMMU."""
 from .approval import ApprovalAuthorizationError, OwnerApproval, OwnerApprovalAuthority
+from .authority import AuthorityMode, AuthorityRule, AuthorityStore
 from .contracts import ControlCommand, ControlDecision, ControlSnapshot
 from .service import OwnerControlCenter
 
@@ -7,6 +8,9 @@ __all__ = [
     "ApprovalAuthorizationError",
     "OwnerApproval",
     "OwnerApprovalAuthority",
+    "AuthorityMode",
+    "AuthorityRule",
+    "AuthorityStore",
     "ControlCommand",
     "ControlDecision",
     "ControlSnapshot",
