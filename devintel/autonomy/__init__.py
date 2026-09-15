a6eeeab04f33fa74356a42a9b6597a5618cdc2d0
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-15 02:55:10 -0400

    feat(autonomy): export operational supervisor

--- devintel/autonomy/__init__.py
+++ devintel/autonomy/__init__.py
@@ -2,7 +2,21 @@
 from .contracts import AutonomousCycle, AutonomyPhase, Observation
 from .engine import AutonomousEngine
 from .store import AutonomousCycleStore
+from .supervisor import AutonomyRun, AutonomyRunPolicy, AutonomousSupervisor
 from .learning import OutcomeEvidence, LearningProposal, OutcomeLearner
 from .learning_store import LearningStore
 
-__all__ = ["AutonomousCycle", "AutonomyPhase", "Observation", "AutonomousEngine", "AutonomousCycleStore", "OutcomeEvidence", "LearningProposal", "OutcomeLearner", "LearningStore"]
+__all__ = [
+    "AutonomousCycle",
+    "AutonomyPhase",
+    "Observation",
+    "AutonomousEngine",
+    "AutonomousCycleStore",
+    "AutonomyRun",
+    "AutonomyRunPolicy",
+    "AutonomousSupervisor",
+    "OutcomeEvidence",
+    "LearningProposal",
+    "OutcomeLearner",
+    "LearningStore",
+]
