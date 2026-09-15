e02f9c5682ff6e3013cbcc2c65c027f74f803de3
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-15 03:51:18 -0400

    feat(domain): export domain intelligence primitives

--- devintel/intelligence/__init__.py
+++ devintel/intelligence/__init__.py
@@ -1,9 +1,11 @@
 """Integrated intelligence capabilities."""
 
+from .domain import DomainIntelligenceEngine, DomainProfile, DomainSignal
 from .knowledge import KnowledgeConflict, KnowledgeIntelligence, KnowledgeItem, KnowledgeQuery
 from .problem_opportunity import ProblemOpportunityCandidate, ProblemOpportunityEngine, ProblemOpportunityResult
 
 __all__ = [
+    "DomainIntelligenceEngine", "DomainProfile", "DomainSignal",
     "KnowledgeConflict", "KnowledgeIntelligence", "KnowledgeItem", "KnowledgeQuery",
     "ProblemOpportunityCandidate", "ProblemOpportunityEngine", "ProblemOpportunityResult",
 ]
