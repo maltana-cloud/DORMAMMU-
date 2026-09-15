a9cce06817437e37042a656dd5594e2092c97c06
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-15 03:46:16 -0400

    feat(discovery): export problem opportunity intelligence

--- devintel/intelligence/__init__.py
+++ devintel/intelligence/__init__.py
@@ -1,5 +1,9 @@
 """Integrated intelligence capabilities."""
 
 from .knowledge import KnowledgeConflict, KnowledgeIntelligence, KnowledgeItem, KnowledgeQuery
+from .problem_opportunity import ProblemOpportunityCandidate, ProblemOpportunityEngine, ProblemOpportunityResult
 
-__all__ = ["KnowledgeConflict", "KnowledgeIntelligence", "KnowledgeItem", "KnowledgeQuery"]
+__all__ = [
+    "KnowledgeConflict", "KnowledgeIntelligence", "KnowledgeItem", "KnowledgeQuery",
+    "ProblemOpportunityCandidate", "ProblemOpportunityEngine", "ProblemOpportunityResult",
+]
