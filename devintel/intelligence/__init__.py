8f3a0bf0316867a63e41d949d4ad3dac3ac5cfd6
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-15 19:03:19 -0400

    feat: export gap mission engine

--- devintel/intelligence/__init__.py
+++ devintel/intelligence/__init__.py
@@ -6,6 +6,7 @@ from .ecosystem import AgentDescriptor, AgentRegistry, CoordinationResult, Coord
 from .global_discovery import DiscoveryRound, GlobalDiscoveryEngine, GlobalDiscoveryPolicy
 from .knowledge import KnowledgeConflict, KnowledgeIntelligence, KnowledgeItem, KnowledgeQuery
 from .problem_opportunity import ProblemOpportunityCandidate, ProblemOpportunityEngine, ProblemOpportunityResult
+from .gap_missions import GapMissionEngine, MissionProposal
 
 __all__ = [
     "CollaborationRequest", "CollaborationResponse", "CollaborationStatus", "HumanCollaborationEngine",
@@ -14,4 +15,5 @@ __all__ = [
     "DiscoveryRound", "GlobalDiscoveryEngine", "GlobalDiscoveryPolicy",
     "KnowledgeConflict", "KnowledgeIntelligence", "KnowledgeItem", "KnowledgeQuery",
     "ProblemOpportunityCandidate", "ProblemOpportunityEngine", "ProblemOpportunityResult",
+    "GapMissionEngine", "MissionProposal",
 ]
