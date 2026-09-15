45443bb2d057a2122c9f8adaae13a24c5a1247df
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-15 19:10:03 -0400

    feat: export mission learning bridge

--- devintel/autonomy/__init__.py
+++ devintel/autonomy/__init__.py
@@ -5,12 +5,15 @@ from .store import AutonomousCycleStore
 from .supervisor import AutonomyRun, AutonomyRunPolicy, AutonomousSupervisor
 from .learning import OutcomeEvidence, LearningProposal, OutcomeLearner
 from .learning_store import LearningStore
+from .mission_learning import LearningTransition, MissionLearningBridge
+from .next_objective import NextObjective, NextObjectiveSelector, ObjectiveCandidate
 from .evolution import EvolutionEngine, EvolutionEvaluation, EvolutionPolicy, ImprovementCandidate, Promotion
 from .evolution_store import EvolutionStore
 
 __all__ = [
     "AutonomousCycle", "AutonomyPhase", "Observation", "AutonomousEngine", "AutonomousCycleStore",
     "AutonomyRun", "AutonomyRunPolicy", "AutonomousSupervisor", "OutcomeEvidence", "LearningProposal",
-    "OutcomeLearner", "LearningStore", "EvolutionEngine", "EvolutionEvaluation", "EvolutionPolicy",
+    "OutcomeLearner", "LearningStore", "LearningTransition", "MissionLearningBridge", "NextObjective",
+    "NextObjectiveSelector", "ObjectiveCandidate", "EvolutionEngine", "EvolutionEvaluation", "EvolutionPolicy",
     "ImprovementCandidate", "Promotion", "EvolutionStore",
 ]
