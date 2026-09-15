0e8f2c50d3a40bda141a7d7a8911ed867cf86b4e
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-15 19:13:01 -0400

    feat: export closed frontier loop

--- devintel/missions/__init__.py
+++ devintel/missions/__init__.py
@@ -4,8 +4,9 @@ from .coordination import MissionLease
 from .engine import MissionRunner, MissionRunPolicy
 from .store import Mission, MissionStatus, MissionStep, MissionStepRecord, MissionStore
 from .executive_bridge import MissionExecutionPolicy, MissionExecutiveBridge
+from .frontier_loop import FrontierLoopResult, MissionFrontierLoop
 
 __all__ = [
     "Mission", "MissionLease", "MissionRunner", "MissionRunPolicy", "MissionStatus", "MissionStep", "MissionStepRecord", "MissionStore",
-    "MissionExecutionPolicy", "MissionExecutiveBridge",
+    "MissionExecutionPolicy", "MissionExecutiveBridge", "FrontierLoopResult", "MissionFrontierLoop",
 ]
