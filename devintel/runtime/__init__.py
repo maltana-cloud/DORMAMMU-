1e1124a5cfe98716df9f097046e80d4958330bba
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-15 10:00:44 -0400

    Ω: expose persistent memory and dynamic authority through runtime API

--- devintel/runtime/__init__.py
+++ devintel/runtime/__init__.py
@@ -5,10 +5,14 @@ from .scheduler import RuntimeScheduler, ScheduledJob
 from .worker import RuntimeJob, RuntimeJobStore, RuntimeWorker, build_worker
 from ..modules.simulation import SimulationRuntimeAdapter
 from ..persistence import KnowledgeRecord, KnowledgeStore, PersistenceSnapshot, StateRecord
+from ..memory import MemoryEntry, MemoryKind, MemoryStore
+from ..control.authority import AuthorityMode, AuthorityRule, AuthorityStore
 
 __all__ = [
     "DORMAMMURuntime", "DEVINTELRuntime", "RuntimeSnapshot",
     "RuntimeScheduler", "ScheduledJob", "RuntimeJob", "RuntimeJobStore",
     "RuntimeWorker", "build_worker", "SimulationRuntimeAdapter",
     "KnowledgeRecord", "KnowledgeStore", "PersistenceSnapshot", "StateRecord",
+    "MemoryEntry", "MemoryKind", "MemoryStore",
+    "AuthorityMode", "AuthorityRule", "AuthorityStore",
 ]
