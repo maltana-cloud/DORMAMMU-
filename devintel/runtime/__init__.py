e2e765feb5fb4dfdbf278ff4687534e71cc9e98e
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-15 03:12:07 -0400

    feat(runtime): expose production orchestration primitives

--- devintel/runtime/__init__.py
+++ devintel/runtime/__init__.py
@@ -1,6 +1,19 @@
 """Public runtime integration API for DORMAMMU."""
 
 from .app import DORMAMMURuntime, DEVINTELRuntime, RuntimeSnapshot
+from .scheduler import RuntimeScheduler, ScheduledJob
+from .worker import RuntimeJob, RuntimeJobStore, RuntimeWorker, build_worker
 from ..modules.simulation import SimulationRuntimeAdapter
 
-__all__ = ["DORMAMMURuntime", "DEVINTELRuntime", "RuntimeSnapshot", "SimulationRuntimeAdapter"]
+__all__ = [
+    "DORMAMMURuntime",
+    "DEVINTELRuntime",
+    "RuntimeSnapshot",
+    "RuntimeScheduler",
+    "ScheduledJob",
+    "RuntimeJob",
+    "RuntimeJobStore",
+    "RuntimeWorker",
+    "build_worker",
+    "SimulationRuntimeAdapter",
+]
