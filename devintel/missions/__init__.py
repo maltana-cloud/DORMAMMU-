486262809d77b1c85c9849de1b4dc7d434049b8d
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-15 19:04:31 -0400

    fix: export durable mission step contract

--- devintel/missions/__init__.py
+++ devintel/missions/__init__.py
@@ -2,10 +2,10 @@
 
 from .coordination import MissionLease
 from .engine import MissionRunner, MissionRunPolicy
-from .store import Mission, MissionStatus, MissionStore
+from .store import Mission, MissionStatus, MissionStep, MissionStepRecord, MissionStore
 from .executive_bridge import MissionExecutionPolicy, MissionExecutiveBridge
 
 __all__ = [
-    "Mission", "MissionLease", "MissionRunner", "MissionRunPolicy", "MissionStatus", "MissionStore",
+    "Mission", "MissionLease", "MissionRunner", "MissionRunPolicy", "MissionStatus", "MissionStep", "MissionStepRecord", "MissionStore",
     "MissionExecutionPolicy", "MissionExecutiveBridge",
 ]
