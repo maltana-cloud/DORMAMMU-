4f218744dbe2244cd90b4a7dbb2b5725f1ab1a6c
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-15 04:15:48 -0400

    feat(ecosystem): export coordination contracts

--- devintel/intelligence/__init__.py
+++ devintel/intelligence/__init__.py
@@ -2,12 +2,14 @@
 
 from .collaboration import CollaborationRequest, CollaborationResponse, CollaborationStatus, HumanCollaborationEngine
 from .domain import DomainIntelligenceEngine, DomainProfile, DomainSignal
+from .ecosystem import AgentDescriptor, AgentRegistry, CoordinationResult, CoordinationTask, EcosystemCoordinator
 from .knowledge import KnowledgeConflict, KnowledgeIntelligence, KnowledgeItem, KnowledgeQuery
 from .problem_opportunity import ProblemOpportunityCandidate, ProblemOpportunityEngine, ProblemOpportunityResult
 
 __all__ = [
     "CollaborationRequest", "CollaborationResponse", "CollaborationStatus", "HumanCollaborationEngine",
     "DomainIntelligenceEngine", "DomainProfile", "DomainSignal",
+    "AgentDescriptor", "AgentRegistry", "CoordinationResult", "CoordinationTask", "EcosystemCoordinator",
     "KnowledgeConflict", "KnowledgeIntelligence", "KnowledgeItem", "KnowledgeQuery",
     "ProblemOpportunityCandidate", "ProblemOpportunityEngine", "ProblemOpportunityResult",
 ]
